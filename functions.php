cf80f9283d6c242b030ef50b2ce0560cde58b940
Fazle Bari <fazlebarisn@gmail.com>
2024-01-09 00:10:17 +0600

    Works

--- functions.php
+++ functions.php
@@ -4,8 +4,6 @@ function mb_product_filter_shortcode()
     ob_start();
 
     // Check if it's a 'mb-category' category page
-
-
     $userLocationMetaKey = isset($_COOKIE['mb_shipping_store']) ? $_COOKIE['mb_shipping_store'] : "T2G 4C2";
 
     $post_id = get_posts(array(
@@ -20,13 +18,12 @@ function mb_product_filter_shortcode()
     $locationId = get_post_meta($post_id, "location_id", true);
     $userSelectedStoreId = "store_" . $locationId;
 
-    // Get all product categories assigned to products in the current 'mb-category'
     if (is_shop()) {
         $args = array(
-            'posts_per_page' => -1,  // Set to -1 to retrieve all posts
+            'posts_per_page' => -1,
             'post_type'      => 'product',
             'meta_query'     => array(
-                'relation' => 'AND', // Both conditions should be true
+                'relation' => 'AND',
                 array(
                     'key'     => $userSelectedStoreId,
                     'value'   => 0,
@@ -43,12 +40,12 @@ function mb_product_filter_shortcode()
         );
     } else {
         $category = get_queried_object();
-        $category_id = $category->term_id;
+        $category_id = absint($category->term_id);
         $args = array(
-            'posts_per_page' => -1,  // Set to -1 to retrieve all posts
+            'posts_per_page' => -1,
             'post_type'      => 'product',
             'meta_query'     => array(
-                'relation' => 'AND', // Both conditions should be true
+                'relation' => 'AND',
                 array(
                     'key'     => $userSelectedStoreId,
                     'value'   => 0,
@@ -65,8 +62,8 @@ function mb_product_filter_shortcode()
             'tax_query'      => array(
                 array(
                     'taxonomy' => 'mb-category',
-                    'field'    => 'id',
-                    'terms'    => $category_id,
+                    'field'    => 'id', 
+                    'terms'    => $category_id, 
                     'operator' => 'IN',
                 ),
             ),
@@ -75,64 +72,60 @@ function mb_product_filter_shortcode()
 
     $products = get_posts($args);
 
-    $product_ids = wp_list_pluck($products, 'ID');
+    // Check if the query was successful before proceeding
+    if (!empty($products)) {
+        $terms = wp_get_object_terms(wp_list_pluck($products, 'ID'), 'filter');
 
-    $terms = wp_get_object_terms($product_ids, 'filter');
-
-
-
-
-    // If there are matching 'filter' categories, display the filter form
-    if (!empty($terms)) {
+        // If there are matching 'filter' categories, display the filter form
+        if (!empty($terms)) {
 ?>
-        <form id="mb-product-filter-form" action="" method="GET">
-            <div class="mb-filter-wrap">
-                <h2 class="mb-filter-title">Filter</h2>
-                <div class="mb-filter-body">
-                    <ul class="product-categories">
-                        <?php
-                        foreach ($terms as $category) {
-                            $children = get_term_children($category->term_id, 'filter');
+            <form id="mb-product-filter-form" action="<?php echo esc_url(home_url($_SERVER['REQUEST_URI'])); ?>" method="GET">
+                <div class="mb-filter-wrap">
+                    <h2 class="mb-filter-title">Filter</h2>
+                    <div class="mb-filter-body">
+                        <ul class="product-categories">
+                            <?php
+                            foreach ($terms as $category) {
+                                $children = get_term_children($category->term_id, 'filter');
 
-                            // Display main category only if it has children
-                            if (!empty($children)) {
-                        ?>
-                                <li class="mb-parrent-cat" id="mb-parrent-cat-<?php echo esc_html($category->slug); ?>">
-                                    <?php echo esc_html($category->name); ?>
-                                </li>
-                                <div class="mb-child-category-<?php echo esc_html($category->slug); ?>">
-                                    <div class="mb-list-group-item">
-                                        <ul class="product-subcategories">
-                                            <?php
-                                            foreach ($children as $child_id) {
-                                                $child = get_term($child_id, 'filter');
-                                            ?>
-
-                                                <li>
-                                                    <input type="checkbox" name="product_category[]" value="<?php echo esc_attr($child->slug); ?>" <?php echo (isset($_GET['product_category']) && in_array($child->slug, $_GET['product_category'])) ? 'checked' : ''; ?>>
-                                                    <?php echo esc_html($child->name); ?>
-                                                </li>
-
-                                            <?php
-                                            }
-                                            ?>
-                                        </ul>
+                                // Display main category only if it has children
+                                if (!empty($children)) {
+                            ?>
+                                    <li class="mb-parrent-cat" id="mb-parrent-cat-<?php echo esc_html($category->slug); ?>">
+                                        <?php echo esc_html($category->name); ?>
+                                    </li>
+                                    <div class="mb-child-category-<?php echo esc_html($category->slug); ?>">
+                                        <div class="mb-list-group-item">
+                                            <ul class="product-subcategories">
+                                                <?php
+                                                foreach ($children as $child_id) {
+                                                    $child = get_term($child_id, 'filter');
+                                                ?>
+                                                    <li>
+                                                        <input type="checkbox" name="filter[]" value="<?php echo esc_attr($child->slug); ?>" <?php echo (isset($_GET['filter']) && in_array($child->slug, $_GET['filter'])) ? 'checked' : ''; ?>>
+                                                        <?php echo esc_html($child->name); ?>
+                                                    </li>
+                                                <?php
+                                                }
+                                                ?>
+                                            </ul>
+                                        </div>
                                     </div>
-                                </div>
-                        <?php
+                            <?php
+                                }
                             }
-                        }
-                        ?>
-                    </ul>
-                </div>
+                            ?>
+                        </ul>
+                    </div>
 
-                <p>
-                    <input type="submit" value="Filter Products">
-                    <input type="reset" value="Reset Filter">
-                </p>
-            </div>
-        </form>
+                    <p>
+                        <input type="submit" value="Filter Products">
+                        <input type="reset" value="Reset Filter">
+                    </p>
+                </div>
+            </form>
 <?php
+        }
     }
 
     // Get the output buffer contents and clean the buffer
@@ -144,21 +137,24 @@ function mb_product_filter_shortcode()
 add_shortcode('mb_product_filter', 'mb_product_filter_shortcode');
 
 // Filter products based on selected categories
-function product_filter_function($query)
-{
+function product_filter_function($query) {
     if (is_admin() || !$query->is_main_query()) {
         return;
     }
 
-    if (is_tax('mb-category') && isset($_GET['product_category'])) {
+    if (isset($_GET['filter'])) {
+        $filter_term = sanitize_text_field($_GET['filter']);
+
         $query->set('tax_query', array(
             array(
                 'taxonomy' => 'filter',
                 'field'    => 'slug',
-                'terms'    => $_GET['product_category'],
+                'terms'    => $filter_term,
                 'operator' => 'IN',
             ),
         ));
     }
 }
+
 add_action('pre_get_posts', 'product_filter_function');
+?>
