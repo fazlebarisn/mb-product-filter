fcbdfcd2574ee58c9eed507eb719b6e0af92e039
Fazle Bari <fazlebarisn@gmail.com>
2024-01-08 17:07:54 +0600

    done

--- assets/js/mb-filter.js
+++ assets/js/mb-filter.js
@@ -1,6 +1,36 @@
-;(function($){
-    $(document).ready(function(){
-        // write jquery codes here
-        console.log('working');
-    });
+(function ($) {
+  $(document).ready(function () {
+
+    // Define a function to handle the click event
+    function toggleChild(parent, child) {
+      var parentElement = $(`li#${parent}`);
+      var childElement = $(`.mb-child-category-${child}`);
+      childElement.hide();
+
+      parentElement.on("click", function () {
+        childElement.toggle();
+      });
+    }
+
+    // Call the function for each pair of parent and child
+    toggleChild("mb-parrent-cat-accessory-type", "accessory-type");
+    toggleChild("mb-parrent-cat-application", "application");
+    toggleChild("mb-parrent-cat-by-category", "by-category");
+    toggleChild("mb-parrent-cat-color", "color");
+    toggleChild("mb-parrent-cat-color-types", "color-types");
+    toggleChild("mb-parrent-cat-hair-needs", "hair-needs");
+    toggleChild("mb-parrent-cat-hair-product-type", "hair-product-type");
+    toggleChild("mb-parrent-cat-hair-type", "hair-type");
+    toggleChild("mb-parrent-cat-lash-color", "lash-color");
+    toggleChild("mb-parrent-cat-lash-type", "lash-type");
+    toggleChild("mb-parrent-cat-length", "length");
+    toggleChild("mb-parrent-cat-nail-color", "nail-color");
+    toggleChild("mb-parrent-cat-nail-finish", "nail-finish");
+    toggleChild("mb-parrent-cat-price", "price");
+    toggleChild("mb-parrent-cat-recipient", "recipient");
+    toggleChild("mb-parrent-cat-size", "size");
+    toggleChild("mb-parrent-cat-skin-concerns", "skin-concerns");
+    toggleChild("mb-parrent-cat-skin-product-type", "skin-product-type");
+    toggleChild("mb-parrent-cat-skin-type", "skin-type");
+  });
 })(jQuery);
--- functions.php
+++ functions.php
@@ -14,29 +14,31 @@ function mb_product_filter_shortcode()
                 ));
 
                 foreach ($categories as $category) {
-                    // Display main category
-                ?>
-                    <li>
-                        <input type="checkbox" name="product_category[]" value="<?php echo esc_attr($category->slug); ?>" <?php echo (isset($_GET['product_category']) && in_array($category->slug, $_GET['product_category'])) ? 'checked' : ''; ?>>
-                        <?php echo esc_html($category->name); ?>
-                    </li>
-
-                    <?php
                     // Get children of the main category
                     $children = get_term_children($category->term_id, 'filter');
 
+                    // Display main category only if it has children
                     if (!empty($children)) {
-                        echo '<ul class="product-subcategories">';
-                        foreach ($children as $child_id) {
-                            $child = get_term($child_id, 'filter');
-                    ?>
-                            <li style="margin-left: 20px;">
-                                <input type="checkbox" name="product_category[]" value="<?php echo esc_attr($child->slug); ?>" <?php echo (isset($_GET['product_category']) && in_array($child->slug, $_GET['product_category'])) ? 'checked' : ''; ?>>
-                                <?php echo esc_html($child->name); ?>
-                            </li>
-                <?php
-                        }
-                        echo '</ul>';
+                        ?>
+                        <li class="mb-parrent-cat" id="mb-parrent-cat-<?php echo esc_html($category->slug); ?>">
+                            <?php echo esc_html($category->name); ?>
+                        </li>
+                        <div class="mb-child-category-<?php echo esc_html($category->slug); ?>">
+                            <ul class="product-subcategories">
+                                <?php
+                                foreach ($children as $child_id) {
+                                    $child = get_term($child_id, 'filter');
+                                    ?>
+                                    <li style="margin-left: 20px;">
+                                        <input type="checkbox" name="product_category[]" value="<?php echo esc_attr($child->slug); ?>" <?php echo (isset($_GET['product_category']) && in_array($child->slug, $_GET['product_category'])) ? 'checked' : ''; ?>>
+                                        <?php echo esc_html($child->name); ?>
+                                    </li>
+                                <?php
+                                }
+                                ?>
+                            </ul>
+                        </div>
+                        <?php
                     }
                 }
                 ?>
@@ -48,7 +50,7 @@ function mb_product_filter_shortcode()
             </p>
         </div>
     </form>
-<?php
+    <?php
     // Get the output buffer contents and clean the buffer
     $output = ob_get_clean();
     return $output;
@@ -67,7 +69,7 @@ function product_filter_function($query)
     if (isset($_GET['product_category'])) {
         $query->set('tax_query', array(
             array(
-                'taxonomy' => 'product_cat',
+                'taxonomy' => 'filter',
                 'field'    => 'slug',
                 'terms'    => $_GET['product_category'],
                 'operator' => 'IN',
@@ -76,3 +78,4 @@ function product_filter_function($query)
     }
 }
 add_action('pre_get_posts', 'product_filter_function');
+
